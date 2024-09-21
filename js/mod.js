24111d6d06ac5a8fedef9443ef93cb5789b25983
FiraFlamez <sb848vstheultraverse@gmail.com>
2024-09-21 13:27:52 -0400

    Add files via upload

---
+++ js/mod.js
@@ -0,0 +1,87 @@
+const mod = {
+    primaryName: "<span class='flipped-v'>Ω</span>",
+    secondaryName: "Layers Layers",
+    version: "1.0.0",
+    engineVer: "1.0.1", //DO NOT MODIFY
+    debugMode: false,
+    Infinities: [new Decimal(2).pow(1024), new Decimal("1.8e30008"), new Decimal("1.8e300000008"), new Decimal("ee38")],
+    themes: [
+        ["Dark", "css/themes/dark.css"],
+        ["Dark Alt (by Jeehan2561)", "css/themes/darkalt.css"],
+        ["Dark Alt Squared (by Fira)", "css/themes/darkaltsquared.css"],
+        ["Modern", "css/themes/modern.css"],
+        ["Light", "css/themes/light.css"],
+        ["Neon", "css/themes/neon.css"],
+        ["Godot Blue", "css/themes/darkblue.css"],
+        ["Halloween", "css/themes/spooky.css"],
+        ["eXPerience", "css/themes/experience.css"]
+    ],
+    layerNames: [
+        ["Everything",
+        [
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZαβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩАА̀А̂А̄ӒБВГҐДЂЃЕЀЕ̄Е̂ЁЄЖЗЗ́ЅИІЇꙆЍИ̂ӢЙЈКЛЉМНЊОО̀О̂ŌӦПРСС́ТЋЌУУ̀У̂ӮЎӰФХЦЧЏШЩꙎЪЪ̀ЫЬѢЭЮЮ̀ЯЯ̀ӐА̊А̃Ӓ̄ӔӘӘ́Ә̃ӚВ̌ԜГ̑Г̇Г̣Г̌Г̂Г̆Г̈г̊ҔҒӺҒ̌ғ̊ӶД́Д̌Д̈Д̣Д̆ӖЕ̃Ё̄Є̈ԐԐ̈ҖӜӁЖ̣ҘӞЗ̌З̣З̆ӠИ̃ӤҊҚӃҠҞҜК̣к̊қ̊ԚᴫЛ́ӅԮԒЛ̈ӍᵸН́ӉҢԨӇҤО̆О̃Ӧ̄ӨӨ̄Ө́Ө̆ӪԤП̈Р̌ҎР̌С̌ҪС̣С̱Т́Т̈Т̌Т̇Т̣ҬᲉУ̃ӲУ̊Ӱ̄ҰҮҮ́Х̣Х̱Х̮Х̑Х̌ҲӼх̊Ӿӿ̊ҺҺ̈ԦЦ̌Ц̈ҴҶҶ̣ӴӋҸЧ̇Ч̣ҼҾШ̣ꚜЫ̆Ы̄ӸꚝҌҨЭ̆Э̄Э̇ӬӬ́Ӭ̄Ю̆Ю̈Ю̄Я̆Я̄Я̈ӀА̨Б̀Б̣Б̱В̀Г̀Г̧Г̄Г̓Г̆Ҕ̀Ҕ̆ԀД̓Д̀Д̨ԂꚀꙢЕ̇Е̨Ж̀Ж̑ꙂꙄЏ̆ꚄꚄ̆ꙀЗ̀З̑ԄԆꚈԪꚂꚔІ̂І̣І̨Ј̵Ј̃ꙈК̓К̀К̆Ӄ̆К̑К̇К̈К̄ԞК̂Л̀ԠꙤԈЛ̑Л̇ԔМ̀М̃ꙦН̀Н̄Н̧Н̃ԊԢН̡ѺꙨꙪꙬꙮꚘꚚП̓П̀П́ҦП̧П̑ҀԚ̆Р́Р̀Р̃ԖС̀С̈ԌҪ̓Т̓Т̀ԎТ̑ꚊТ̧ꚌꚌ̆ѸꙊУ̇У̨ꙋ́Ф̑Ф̓Х́Х̀Х̆Х̇Х̧Х̾Х̓һ̱ѠꙌѼѾꙠЦ̀Ц́Ц̓ꚎꚎ̆ꚐЧ́Ч̀Ч̆Ч̑Ч̓ԬꚒꚆꚆ̆Ҽ̆Ш̀Ш̆Ш̑Щ̆ꚖꚖ̆Ъ̄Ъ̈Ъ̈̄ꙐЫ̂Ы̃Ѣ́Ѣ̈Ѣ̆ꙒЭ̨Э̂ꙔЮ̂ꙖЯ̈Я̂Я̨ԘѤѦꙘѪꙚѨꙜѬѮѰѲѴѶꙞ",
+            "ψϝϛͱϻϙͳϸðþȝƿəŋſÐÞȜǷƏŊꙂꙀѪѬѦѨ",
+            ["<span class='flipped-v'>Ω</span>", "<span class='flipped-v'>Ω</span><sup>2</sup>","<span class='flipped-v'>Ω</span><sup>3</sup>","<span class='flipped-v'>Ω</span><sup>2<sup>2</sup></sup>"]
+        ]],
+        ["Greek",
+        [
+            "αβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ",
+            "ψϝϛͱϻϙͳϸ",
+            ["<span class='flipped-v'>Ω</span>", "<span class='flipped-v'>Ω</span><sup>2</sup>","<span class='flipped-v'>Ω</span><sup>3</sup>","<span class='flipped-v'>Ω</span><sup>2<sup>2</sup></sup>"]
+        ]],
+        ["Latin",
+        [
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "ðþȝƿəŋſÐÞȜǷƏŊ",
+            "æœĳǉ"
+        ]],
+        ["Alphabet",
+        [
+            "abcdefghijklmnopqrstuvwxyz",
+            "123456789",
+            "ABCD"
+        ]],
+        ["Symbols",
+        [
+            '!"£$%^&*;:@',
+            "<,[{}].>",
+            "+×÷^"
+        ]],
+        ["Binary",
+        [
+            '01',
+            "01",
+            "2345"
+        ]],
+        ["Random",
+        [
+            Utils.createRandomWord(10, new Random(Date.now()).nextInt()),
+            Utils.createRandomWord(10, new Random(Math.floor(Date.now()/2)).nextInt()),
+            [Utils.createRandomWord(2, new Random(Math.floor(Date.now()/3)).nextInt()),Utils.createRandomWord(3, new Random(Math.floor(Date.now()/4)).nextInt()),Utils.createRandomWord(4, new Random(Math.floor(Date.now()/5)).nextInt()),Utils.createRandomWord(5, new Random(Math.floor(Date.now()/6)).nextInt())]
+        ]]
+    ],
+    layerNamePlaceholder: "𐌒",
+    fonts: [
+        ["Monospace Typewriter", "css/fonts/typespace.css"],
+        ["Source Code Pro", "css/fonts/scp.css"],
+        ["Comic Sans", "css/fonts/comic.css"],
+        ["Arial", "css/fonts/arial.css"],
+        ["Roboto", "css/fonts/roboto.css"],
+        ["Ubuntu", "css/fonts/ubuntu.css"],
+        ["Comfortaa", "css/fonts/comfortaa.css"],
+        ["Minecraft", "css/fonts/minecraft.css"],
+    ],
+    saves: [
+        ["Save 1", ""],
+        ["Save 2", "2"],
+        ["Save 3", "3"],
+        ["Save 4", "4"],
+    ],
+    debugClasses: []
+}
+
+//DO NOT MODIFY CODE PAST THIS POINT AS IT IS NEEDED (unless your a pro coder then do some experimenting)
+
+mod.layerNames.push(["Refresh Names", "refresh"])
+
+document.getElementById("superImportantTitle").innerHTML = "<span class='omega'>"+mod.primaryName+"</span>"+mod.secondaryName
